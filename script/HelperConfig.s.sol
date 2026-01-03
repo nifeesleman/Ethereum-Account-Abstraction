d5dfa91be674395bc6ff50c82e6dded9e0ecc20c
nifeesleman <nsr-1868-14@amu.edu.et>
2026-01-03 21:35:32 +0300

    Add Arbitrum mainnet configuration to HelperConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -19,6 +19,7 @@ contract HelperConfig is Script {
     uint256 constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
     uint256 constant LOCAL_CHAIN_ID = 31337; // Anvil default
+    uint256 constant ARBITRUM_MAINNET_CHAIN_ID = 42161;
 
     // Official Sepolia EntryPoint address: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789
     // address constant FOUNDRY_DEFAULT_WALLET = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
@@ -32,10 +33,17 @@ contract HelperConfig is Script {
     constructor() {
         networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getEthSepoliaConfig();
         networkConfigs[ZKSYNC_SEPOLIA_CHAIN_ID] = getZkSyncSepoliaConfig();
+        networkConfigs[ARBITRUM_MAINNET_CHAIN_ID] = getArbitrumMainnetConfig();
     }
 
     function getEthSepoliaConfig() public pure returns (NetworkConfig memory) {
         return NetworkConfig({entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789, account: BURNER_WALLET});
+    }
+    function getArbitrumMainnetConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({
+            entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789, // Example EntryPoint on Arbitrum
+            account: BURNER_WALLET
+        });
     }
 
     function getZkSyncSepoliaConfig() public pure returns (NetworkConfig memory) {
