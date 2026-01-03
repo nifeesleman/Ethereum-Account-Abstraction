dfb13806fefda0377bec51b3e549e20d9e5dea78
nifeesleman <nsr-1868-14@amu.edu.et>
2026-01-03 22:57:52 +0300

    Implement deployMinimalAccount functionality in DeployMinimal script; update run method and ownership transfer logic

--- script/DeployMinimal.s.sol
+++ script/DeployMinimal.s.sol
@@ -7,14 +7,18 @@ import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployMinimal is Script {
-    function run() public {}
+    function run() public {
+        deployMinimalAccount();
+    }
 
     function deployMinimalAccount() public returns (HelperConfig, MinimalAccount) {
         HelperConfig helperConfig = new HelperConfig();
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
-        vm.startBroadcast(config.account);
+        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.addr(deployerKey);
+        vm.startBroadcast(deployerKey);
         MinimalAccount minimalAccount = new MinimalAccount(config.entryPoint);
-        minimalAccount.transferOwnership(config.account);
+        minimalAccount.transferOwnership(deployer);
         vm.stopBroadcast();
         return (helperConfig, minimalAccount);
     }
